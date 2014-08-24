7f5502b1e6fa40fcdabf45dc715d87bf6028eb22
Antonino Porcino <nino.porcino@gmail.com>
2014-08-24 13:16:59 +0200

    Cleaned repo

--- Dart_RaytracerDemo/web/dart_raytracerdemo.dart
+++
@@ -1,9 +0,0 @@
-// Converted to Dart by Antonino Porcino, Aug 22, 2014
-// nino.porcino@gmail.com
-
-import 'SimpleRaytracer.dart';
-
-void main()
-{
-  RayTracer.Main();     
-}
--- README.md
+++
@@ -1,4 +0,0 @@
-# Dart.RayTracerDemo
-
-For details see this [blog post entry](http://ninoporcino.blogspot.it/2014/08/dart-ray-tracer-benchmark-compared-to-c.html)
-
